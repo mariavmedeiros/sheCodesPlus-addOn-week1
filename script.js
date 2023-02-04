d083c1c24484c00bce83ef25d76dc6dfb6181ab1
Maria Medeiros <maitevelori@hotmail.com>
2023-02-03 19:37:52 -0800

    updating

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-let brazilTime = moment()
-  .tz("America/Sao_Paulo")
-  .format("dddd, MMMM D, YYYY, H:M a");
-let usaTime = moment()
-  .tz("America/Los_Angeles")
-  .format("dddd, MMMM D, YYYY, H:m A");
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", showTime);
 
-let italyTime = moment().tz("Europe/Rome").format("dddd, MMMM D, YYYY, H:m A)");
+function showTime(event) {
+  let timeZone = event.target.value;
+  let currentTime = moment().tz(timeZone).format("dddd, MMMM D, YYYY H:mm");
 
-function showSelectedCity(event) {
-  if (event.target.value === "saopaulo") {
-    alert(`your current time zone is ${brazilTime}`);
-  }
-  if (event.target.value === "sanfrancisco") {
-    alert(`your current time zone is ${usaTime}`);
-  }
-  if (event.target.value === "rome") {
-    alert(`your current time zone is ${italyTime}`);
+  if (event.target.value.length > 0) {
+    alert(`It is ${currentTime} in ${timeZone}`);
   }
 }
 
-let citySelect = document.querySelector("#cities");
-citySelect.addEventListener("change", showSelectedCity);
+//let brazilTime = moment()
+// .tz("America/Sao_Paulo")
+// .format("dddd, MMMM D, YYYY, H:M a");
+//let brazilElement = document.querySelector("#saopaulo");
+
+//let usaTime = moment()
+// .tz("America/Los_Angeles")
+//.format("dddd, MMMM D, YYYY, H:m A");
+
+//let italyTime = moment().tz("Europe/Rome").format("dddd, MMMM D, YYYY, H:m A)");
+
+//function showSelectedCity(event) {
+//if (event.target.value === "saopaulo") {
+//   alert(`your current time zone is ${brazilTime}`);
+//}
+//if (event.target.value === "sanfrancisco") {
+// alert(`your current time zone is ${usaTime}`);
+//}
+// if (event.target.value === "rome") {
+//  alert(`your current time zone is ${italyTime}`);
+//}
+//}
+
+//let citySelect = document.querySelector("#cities");
+//citySelect.addEventListener("change", showSelectedCity);
