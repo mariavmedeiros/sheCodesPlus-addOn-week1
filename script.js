693986a1cac3d761a1a6e1666b47f659a774e690
Maria Medeiros <maitevelori@hotmail.com>
2023-02-03 19:44:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-let citySelect = document.querySelector("#city");
-citySelect.addEventListener("change", showTime);
-
-function showTime(event) {
-  let timeZone = event.target.value;
-  let currentTime = moment().tz(timeZone).format("dddd, MMMM D, YYYY H:mm");
-
-  if (event.target.value.length > 0) {
-    alert(`It is ${currentTime} in ${timeZone}`);
-  }
-}
-
 //let brazilTime = moment()
 // .tz("America/Sao_Paulo")
 // .format("dddd, MMMM D, YYYY, H:M a");
@@ -21,17 +9,17 @@ function showTime(event) {
 
 //let italyTime = moment().tz("Europe/Rome").format("dddd, MMMM D, YYYY, H:m A)");
 
-//function showSelectedCity(event) {
-//if (event.target.value === "saopaulo") {
-//   alert(`your current time zone is ${brazilTime}`);
-//}
-//if (event.target.value === "sanfrancisco") {
-// alert(`your current time zone is ${usaTime}`);
-//}
-// if (event.target.value === "rome") {
-//  alert(`your current time zone is ${italyTime}`);
-//}
-//}
+function showSelectedCity(event) {
+  if (event.target.value === "saopaulo") {
+    alert(`your current time zone is ${brazilTime}`);
+  }
+  if (event.target.value === "sanfrancisco") {
+    alert(`your current time zone is ${usaTime}`);
+  }
+  if (event.target.value === "rome") {
+    alert(`your current time zone is ${italyTime}`);
+  }
+}
 
-//let citySelect = document.querySelector("#cities");
-//citySelect.addEventListener("change", showSelectedCity);
+let citySelect = document.querySelector("#cities");
+citySelect.addEventListener("change", showSelectedCity);
